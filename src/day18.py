a3bbb3f763d864b8dff5dab81bff3056327870c2
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-12-19 00:30:32 +0000

    Day 18 (now we're talking)

--- src/day18.py
+++ src/day18.py
@@ -1,7 +1,8 @@
 """Day 18: Lavaduct Lagoon"""
 from dataclasses import dataclass
 from enum import Enum
-from typing import Callable
+from functools import cache
+from shapely import Polygon  # type: ignore
 
 
 class Direction(Enum):
@@ -13,62 +14,58 @@ class Direction(Enum):
     UP = "U"
 
 
+@cache
+def get_directions() -> list[Direction]:
+    """Returns a list of all directions"""
+    return list(iter(Direction))
+
+
 @dataclass
 class DigStep:
     """Dig Step Class"""
 
     direction: Direction
     distance: int
-    color: int
 
 
 def parse_line_part1(string: str) -> DigStep:
     """Parses a line according to Part 1's instructions"""
-    parts = string.split()
-    return DigStep(
-        direction=Direction(parts[0]),
-        distance=int(parts[1]),
-        color=int(parts[2][2:-1], base=16),
-    )
-
-
-DIRECTIONS = list(iter(Direction))
+    direction, distance, _ = string.split()
+    direction = Direction(direction)
+    distance = int(distance)
+    return DigStep(direction=direction, distance=distance)
 
 
 def parse_line_part2(string: str) -> DigStep:
     """Parses a line according to Part 2's instructions"""
-    parts = string.split()
-    color = parts[2][2:-1]
-    return DigStep(
-        direction=DIRECTIONS[int(color[-1])],
-        distance=int(color[:-1], base=16),
-        color=0,
-    )
+    _, _, color = string.split()
+    color = color[2:-1]
+    direction = get_directions()[int(color[-1])]
+    distance = int(color[:-1], base=16)
+    return DigStep(direction=direction, distance=distance)
 
 
-ParseFunc = Callable[[str], DigStep]
-
-
-def get_area(lines: list[str], parse_func: ParseFunc) -> int:
-    """Computes the area of the lava lagoon"""
-    inside_sum = 0
-    length = 0
-    y = 0
-    for line in lines:
-        step = parse_func(line)
-        length += step.distance
+def dig(steps: list[DigStep]) -> Polygon:
+    """Digs a path in the lava lagoon"""
+    path: list[tuple[int, int]] = [(0, 0)]
+    for step in steps:
+        curr_pos = path[-1]
         match step.direction:
             case Direction.LEFT:
-                inside_sum += y * step.distance
+                next_pos = curr_pos[0] - step.distance, curr_pos[1]
             case Direction.RIGHT:
-                inside_sum -= y * step.distance
+                next_pos = curr_pos[0] + step.distance, curr_pos[1]
             case Direction.UP:
-                y -= step.distance
+                next_pos = curr_pos[0], curr_pos[1] + step.distance
             case Direction.DOWN:
-                y += step.distance
+                next_pos = curr_pos[0], curr_pos[1] - step.distance
+        path.append(next_pos)
+    return Polygon(path)  # type: ignore
 
-    area = abs(inside_sum) + length // 2 + 1
-    return area
+
+def compute_area(polygon: Polygon) -> int:
+    """Computes the area of a polygon"""
+    return int(polygon.area + polygon.length // 2 + 1)
 
 
 def main() -> None:
@@ -77,10 +74,14 @@ def main() -> None:
         content = f.read()
 
     lines = content.splitlines()
-    parts: list[ParseFunc] = [parse_line_part1, parse_line_part2]
 
-    for i, parse_func in enumerate(parts, 1):
-        print(f"Size of the lagoon (Part {i}): {get_area(lines, parse_func)}")
+    steps = list(map(parse_line_part1, lines))
+    polygon = dig(steps)
+    print(f"Size of the lagoon (Part 1): {compute_area(polygon)}")
+
+    steps = list(map(parse_line_part2, lines))
+    polygon = dig(steps)
+    print(f"Size of the lagoon (Part 2): {compute_area(polygon)}")
 
 
 if __name__ == "__main__":
